c34b4ff9ee8ba7b2a1cdfa12c27ade0a2da4680e
V-Kaidash <dev.kaidash@gmail.com>
2022-12-04 11:22:30 +0200

    Added simple styles

---
+++ src/component/book/BookFilter.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/system";
+import React from "react";
+import { Paper } from '@mui/material';
+
+const BookFilter = () => {
+    return(
+        <Box>
+            <Paper>
+                Book filter
+            </Paper>
+        </Box>
+    );
+}
+
+export default BookFilter;
